9014e5dc0f5f0abea14332c9dd742c2a2364fa8c
joker69216 <hamaraad883@gmail.com>
2023-05-29 15:17:16 +0300

    mixing node and express

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-
-const app = express();
+const http = require('http');
+const app = require('./app');
 
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
