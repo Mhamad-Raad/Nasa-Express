1564731004a21b1e9a8c463ed8d9e3b712c1a603
joker69216 <hamaraad883@gmail.com>
2023-06-05 21:38:05 +0300

    add a new function to add launchers

--- server/src/model/launches.model.js
+++ server/src/model/launches.model.js
@@ -1,7 +1,9 @@
 const launches = new Map();
 
+let latestFlightNumber = 100
+
 const launch = {
-  flightNumber: 100,
+  flightNumber: latestFlightNumber,
   mission: 'Kempler Exploration X',
   rocket: 'Explorer IS1',
   lunchDate: new Date('December 27, 2030 14:00:00'),
@@ -17,6 +19,21 @@ function getAllLaunchesData() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(flight) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['ZTM', 'NASA'],
+      flightNumber: latestFlightNumber,
+      ...flight,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunchesData,
+  addNewLaunch,
 };
