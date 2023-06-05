4685cb3719ca84685efa6a9c45141a06cbbcc86e
joker69216 <hamaraad883@gmail.com>
2023-06-05 21:41:04 +0300

    add controller function

--- server/src/router/launches/launches.controller.js
+++ server/src/router/launches/launches.controller.js
@@ -1,7 +1,26 @@
-const { getAllLaunchesData } = require('../../model/launches.model');
+const { getAllLaunchesData, addNewLaunch } = require('../../model/launches.model');
 
 const getAllLaunches = (req, res) => {
   return res.json(getAllLaunchesData());
 };
+
+const httpAddNewLaunch = (req, res) => {
+  const launch = req.body;
+  launch.launchDate = new Date(launch.launchDate);
+  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+    return res.status(400).json({
+      error: 'Missing required launch property',
+    });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: 'Invalid launch date',
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
 
 module.exports = { getAllLaunches };
